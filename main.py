343cbdd175b9a84ee58cd65b99103ab07f7c573e
Gustavo Cristian <diamonddyaghas@gmail.com>
2022-10-04 12:40:22 -0300

    S-box adicionada (Não revisada)

--- main.py
+++ main.py
@@ -1,9 +1,60 @@
 user_input = "Two One Nine Two"
 encryption_key = "Thats my Kung Fu"
 
+s_box_map = {
+    '00': '63', '01': '7c', '02': '77', '03': '7b', '04': 'f2', '05': '6b', '06': '6f', '07': 'c5', '08': '30',
+    '09': '01', '0a': '67', '0b': '2b', '0c': 'fe', '0d': 'd7', '0e': 'ab', '0f': '76',  # linha 0
+
+    '10': 'ca', '11': '82', '12': 'c9', '13': '7d', '14': 'fa', '15': '59', '16': '47', '17': 'f0', '18': 'ad',
+    '19': 'd4', '1a': 'a2', '1b': 'af', '1c': '9c', '1d': 'a4', '1e': '72', '1f': 'c0',  # linha 1
+
+    '20': 'b7', '21': 'fd', '22': '93', '23': '26', '24': '36', '25': '3f', '26': 'f7', '27': 'cc', '28': '34',
+    '29': 'a5', '2a': 'e5', '2b': 'f1', '2c': '71', '2d': 'd8', '2e': '31', '2f': '15',  # linha 2
+
+    '30': '04', '31': 'c7', '32': '23', '33': 'c3', '34': '18', '35': '96', '36': '05', '37': '9a', '38': '07',
+    '39': '12', '3a': '80', '3b': 'e2', '3c': 'eb', '3d': '27', '3e': 'b2', '3f': '75',  # linha 3
+
+    '40': '09', '41': '83', '42': '2c', '43': '1a', '44': '1b', '45': '6e', '46': '5a', '47': 'a0', '48': '52',
+    '49': '3b', '4a': 'd6', '4b': 'b3', '4c': '29', '4d': 'e3', '4e': '2f', '4f': '84',  # linha 4
+
+    '50': '53', '51': 'd1', '52': '00', '53': 'ed', '54': '20', '55': 'fc', '56': 'b1', '57': '5b', '58': '6a',
+    '59': '4c', '5a': 'be', '5b': '39', '5c': '4a', '5d': '4c', '5e': '58', '5f': 'cf',  # linha 5
+
+    '60': 'd0', '61': 'ef', '62': 'aa', '63': 'fb', '64': '43', '65': '4d', '66': '33', '67': '85', '68': '45',
+    '69': 'f9', '6a': '02', '6b': '7f', '6c': '50', '6d': '3c', '6e': '9f', '6f': 'a8',  # linha 6
+
+    '70': '51', '71': 'a3', '72': '40', '73': '8f', '74': '92', '75': '9d', '76': '38', '77': 'f5', '78': 'bc',
+    '79': 'b6', '7a': 'da', '7b': '21', '7c': '10', '7d': 'ff', '7e': 'f3', '7f': 'd2',  # linha 7
+
+    '80': 'cd', '81': '0c', '82': '13', '83': 'ec', '84': '5f', '85': '97', '86': '44', '87': '17', '88': 'c4',
+    '89': 'a7', '8a': '7e', '8b': '3d', '8c': '64', '8d': '5d', '8e': '19', '8f': '73',  # linha 8
+
+    '90': '60', '91': '81', '92': '4f', '93': 'dc', '94': '22', '95': '2a', '96': '90', '97': '88', '98': '46',
+    '99': 'ee', '9a': 'b8', '9b': '14', '9c': 'de', '9d': '5e', '9e': '0b', '9f': 'db',  # linha 9
+
+    'a0': 'e0', 'a1': '32', 'a2': '3a', 'a3': '0a', 'a4': '49', 'a5': '06', 'a6': '24', 'a7': '5c', 'a8': 'c2',
+    'a9': 'd3', 'aa': 'ac', 'ab': '62', 'ac': '91', 'ad': '95', 'ae': 'e4', 'af': '79',  # linha 10 a
+
+    'b0': 'e7', 'b1': 'c8', 'b2': '37', 'b3': '6d', 'b4': '8d', 'b5': 'd5', 'b6': '4e', 'b7': 'a9', 'b8': '6c',
+    'b9': '56', 'ba': 'f4', 'bb': 'ea', 'bc': '65', 'bd': '7a', 'be': 'ae', 'bf': '08',  # linha b
+
+    'c0': 'ba', 'c1': '78', 'c2': '25', 'c3': '2e', 'c4': '1c', 'c5': 'a6', 'c6': 'b4', 'c7': 'c6', 'c8': 'e8',
+    'c9': 'dd', 'ca': '74', 'cb': '1f', 'cc': '4b', 'cd': 'bd', 'ce': '8b', 'cf': '8a',  # linha c
+
+    'd0': '70', 'd1': '3e', 'd2': 'b5', 'd3': '66', 'd4': '48', 'd5': '03', 'd6': 'f6', 'd7': '0e', 'd8': '61',
+    'd9': '35', 'da': '57', 'db': 'b9', 'dc': '86', 'dd': 'c1', 'de': '1d', 'df': '9e',  # linha d
+
+    'e0': 'e1', 'e1': 'f8', 'e2': '98', 'e3': '11', 'e4': '69', 'e5': 'd9', 'e6': '8e', 'e7': '94', 'e8': '9b',
+    'e9': '1e', 'ea': '87', 'eb': 'e9', 'ec': 'ce', 'ed': '55', 'ee': '28', 'ef': 'df',  # linha e
+
+    'f0': '8c', 'f1': 'a1', 'f2': '89', 'f3': '0d', 'f4': 'bf', 'f5': 'e6', 'f6': '42', 'f7': '68', 'f8': '41',
+    'f9': '99', 'fa': '2d', 'fb': '0f', 'fc': 'b0', 'fd': '54', 'fe': 'bb', 'ff': '16',  # linha f
+
+}
+
 
 def convert_to_hex(var):
-    var = format(ord(var), "x")
+    var = format(ord(var), "02x")
     return var
 
 
@@ -47,19 +98,34 @@ def to_hex_array(input_string):
 
 
 def add_round_key(array, key):
-    c = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    main_array = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 
     for i in range(0, 4):
         for j in range(0, 4):
             # Adiciona a round key através de um XOR com o input
-            c[i][j] = int(array[0][i][j], 16) ^ int(key[0][i][j], 16)
-            c[i][j] = chr(c[i][j])
-            c[i][j] = format(ord(str(c[i][j])), "x")
-    print(f"hex: {c}")
+            main_array[i][j] = int(array[0][i][j], 16) ^ int(key[0][i][j], 16)
+            main_array[i][j] = chr(main_array[i][j])
+            main_array[i][j] = format(ord(str(main_array[i][j])), "x")
+    print(f"hex: {main_array}")
+    return main_array
+
+
+def sub_byte(array, s_box):
+    index = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    for i in range(0, 4):
+        for j in range(0, 4):
+            # transforma '0' em '00' para possibilitar o sub-byte na s-box
+            if array[i][j] == '0':
+                array[i][j] = int(array[i][j], 16)
+                array[i][j] = format(array[i][j], "02x")
+            index[i][j] = array[i][j]
+            print(s_box[index[i][j]])
 
 
 user_input = to_hex_array(user_input)
 
 encryption_key = to_hex_array(encryption_key)
 
-add_round_key(user_input, encryption_key)
+main_array = add_round_key(user_input, encryption_key)
+
+sub_byte(main_array, s_box_map)
