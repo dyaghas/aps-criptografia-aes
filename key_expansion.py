69178eee7dcfce1b4a56bdf4b58c62562fbcb75e
Gustavo Cristian <diamonddyaghas@gmail.com>
2022-10-20 19:13:17 -0300

    problema com o Rcon resolvido

--- key_expansion.py
+++ key_expansion.py
@@ -26,7 +26,7 @@ def rcon(sub_key):
     # Previne que o input seja alterado
     state_res = copy.deepcopy(sub_key)
     rcon_res = int(sub_key[0][0][3], 16)
-    rcon_res += 1
+    rcon_res = rcon_res ^ 1
     rcon_res = f"{rcon_res:x}"
     state_res[0][0][3] = rcon_res
     print(f"rcon:\n{state_res}\n")
@@ -37,12 +37,12 @@ def expansion_xor(rcon_key, original_key):
     state_res = copy.deepcopy(rcon_key)
     # faz o XOR da primeira coluna da chave retornada na função rcon com a chave original
     for e in range(0, 4):
-        print(f"{rcon_key[0][e][0]} xor {rcon_key[0][e][3]}")
         element = int(rcon_key[0][e][0], 16) ^ int(rcon_key[0][e][3], 16)
         # guarda o elemento do cálculo anterior no índice especificado
         state_res[0][e][0] = f"{element:x}"
     for x in range(1, 4):
         for y in range(0, 4):
+            print(f"{state_res[0][y][x - 1]} xor {original_key[0][y][x]}")
             # faz o XOR das três colunas restantes
             element = int(state_res[0][y][x - 1], 16) ^ int(original_key[0][y][x], 16)
             element = f"{element:x}"
